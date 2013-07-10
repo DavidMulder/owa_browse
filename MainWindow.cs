c6cdbdd5355fc0e64a707272a522b84dba98fc28
David Mulder <davidmmulder@gmail.com>
2013-07-10 16:06:06 -0600

    Created seperate file for webkit enhancements. Started working on WebKit.DOMDocument stuff.

--- MainWindow.cs
+++ MainWindow.cs
@@ -9,22 +9,22 @@ using System.Text.RegularExpressions;
 public partial class MainWindow: Gtk.Window
 {
 	private Timer notifications = new Timer(2000);
-	private ExposedWebView webView;
+	private ExtendedWebView webView;
 	private bool recent_notification = false;
 
 	public MainWindow (string url): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		webView = new ExposedWebView();
-		ExposedWebSettings settings = new ExposedWebSettings();
+		webView = new ExtendedWebView();
+		ExtendedWebSettings settings = new ExtendedWebSettings();
 		settings.g_object_set("user-agent", new GLib.Value("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0"));
 		settings.g_object_set("enable-spell-checking", new GLib.Value(true));
 		webView.TitleChanged += HandleTitleChanged;
 		webView.Settings = settings;
 		webView.NewWindowPolicyDecisionRequested += HandleNewWindowPolicyDecisionRequested;
 		webView.CreateWebView += HandleCreateWebView;
-		notifications.Elapsed += HandleElapsed;
-		notifications.Start();
+		//notifications.Elapsed += HandleElapsed;
+		//notifications.Start();
 		webView.Open(url);
 		VBox vbox1 = new VBox();
 		vbox1.PackStart(webView);
@@ -54,6 +54,7 @@ public partial class MainWindow: Gtk.Window
 			VBox container = (VBox)self.Parent;
 			Window parent = (Window)container.Parent;
 			parent.Destroy();
+			args.RetVal = 1;
 		}
 	}
 	
@@ -89,202 +90,3 @@ public partial class MainWindow: Gtk.Window
 		a.RetVal = true;
 	}
 }
-
-class ExposedWebSettings : WebKit.WebSettings {
-	public void g_object_set(string name, GLib.Value value) {
-		SetProperty(name, value);
-	}
-
-	public GLib.Value g_object_get(string name) {
-		return GetProperty(name);
-	}
-}
-
-class ExposedWebView : WebKit.WebView {
-	public event CreateWebViewHandler CreateWebView
-	{
-		add
-		{
-			Signal signal = Signal.Lookup (this, "create-web-view", typeof(CreateWebViewArgs));
-			signal.AddDelegate (value);
-		}
-		remove
-		{
-			Signal signal = Signal.Lookup (this, "create-web-view", typeof(CreateWebViewArgs));
-			signal.RemoveDelegate (value);
-		}
-	}
-
-	public event WebViewReadyHandler WebViewReady
-	{
-		add
-		{
-			Signal signal = Signal.Lookup (this, "web-view-ready", typeof(WebViewReadyArgs));
-			signal.AddDelegate (value);
-		}
-		remove
-		{
-			Signal signal = Signal.Lookup (this, "web-view-ready", typeof(WebViewReadyArgs));
-			signal.RemoveDelegate (value);
-		}
-	}
-
-	public event NewWindowPolicyDecisionRequestedHandler NewWindowPolicyDecisionRequested
-	{
-		add
-		{
-			Signal signal = Signal.Lookup (this, "new-window-policy-decision-requested", typeof(NewWindowPolicyDecisionRequestedArgs));
-			signal.AddDelegate (value);
-		}
-		remove
-		{
-			Signal signal = Signal.Lookup (this, "new-window-policy-decision-requested", typeof(NewWindowPolicyDecisionRequestedArgs));
-			signal.RemoveDelegate (value);
-		}
-	}
-
-	[DefaultSignalHandler (Type = typeof(WebView), ConnectionMethod = "OverrideCreateWebView")]
-	protected virtual WebView OnCreateWebView (WebFrame frame)
-	{
-		ExposedWebView webView = new ExposedWebView();
-		Value empty = Value.Empty;
-		ValueArray valueArray = new ValueArray (2u);
-		Value[] array = new Value[2];
-		array [0] = new Value (this);
-		valueArray.Append (array [0]);
-		array [1] = new Value (frame);
-		valueArray.Append (array [1]);
-		GLib.Object.g_signal_chain_from_overridden (valueArray.ArrayPtr, ref empty);
-		Value[] array2 = array;
-		for (int i = 0; i < array2.Length; i++)
-		{
-			Value value = array2 [i];
-			value.Dispose ();
-		}
-		return webView;
-	}
-
-	[DefaultSignalHandler (Type = typeof(WebView), ConnectionMethod = "OverrideNewWindowPolicyDecisionRequested")]
-	protected virtual int OnNewWindowPolicyDecisionRequested (WebFrame frame, NetworkRequest request, WebNavigationAction navigation_action, WebPolicyDecision policy_decision)
-	{
-		Value val = new Value (GType.Int);
-		ValueArray valueArray = new ValueArray (3u);
-		Value[] array = new Value[5];
-		array [0] = new Value (this);
-		valueArray.Append (array [0]);
-		array [1] = new Value (frame);
-		valueArray.Append (array [1]);
-		array [2] = new Value (request);
-		valueArray.Append (array [2]);
-		array [3] = new Value (navigation_action);
-		valueArray.Append (array [3]);
-		array [4] = new Value (policy_decision);
-		valueArray.Append (array [4]);
-		GLib.Object.g_signal_chain_from_overridden (valueArray.ArrayPtr, ref val);
-		Value[] array2 = array;
-		for (int i = 0; i < array2.Length; i++)
-		{
-			Value value = array2 [i];
-			value.Dispose ();
-		}
-		int result = (int)val;
-		val.Dispose ();
-		return result;
-	}
-
-	[DefaultSignalHandler (Type = typeof(WebView), ConnectionMethod = "OverrideWebViewReady")]
-	protected virtual bool OnWebViewReady (WebFrame frame)
-	{
-		Value empty = Value.Empty;
-		ValueArray valueArray = new ValueArray (2u);
-		Value[] array = new Value[2];
-		array [0] = new Value (this);
-		valueArray.Append (array [0]);
-		array [1] = new Value (frame);
-		valueArray.Append (array [1]);
-		GLib.Object.g_signal_chain_from_overridden (valueArray.ArrayPtr, ref empty);
-		Value[] array2 = array;
-		for (int i = 0; i < array2.Length; i++)
-		{
-			Value value = array2 [i];
-			value.Dispose ();
-		}
-		return true;
-	}
-}
-
-public delegate void WebViewReadyHandler (object o, WebViewReadyArgs args);
-
-public class WebViewReadyArgs : SignalArgs
-{
-	//
-	// Properties
-	//
-	
-	public WebFrame Frame
-	{
-		get
-		{
-			return (WebFrame)base.Args [0];
-		}
-	}
-}
-
-public delegate void CreateWebViewHandler (object o, CreateWebViewArgs args);
-
-public class CreateWebViewArgs : SignalArgs
-{
-	//
-	// Properties
-	//
-	
-	public WebFrame Frame
-	{
-		get
-		{
-			return (WebFrame)base.Args [0];
-		}
-	}
-}
-
-public delegate void NewWindowPolicyDecisionRequestedHandler (object o, NewWindowPolicyDecisionRequestedArgs args);
-
-public class NewWindowPolicyDecisionRequestedArgs : SignalArgs
-{
-	//
-	// Properties
-	//
-	
-	public WebFrame Frame
-	{
-		get
-		{
-			return (WebFrame)base.Args [0];
-		}
-	}
-	
-	public NetworkRequest Request
-	{
-		get
-		{
-			return (NetworkRequest)base.Args [1];
-		}
-	}
-
-	public WebNavigationAction NavigationAction
-	{
-		get
-		{
-			return (WebNavigationAction)base.Args [2];
-		}
-	}
-
-	public WebPolicyDecision PolicyDecision
-	{
-		get
-		{
-			return (WebPolicyDecision)base.Args [3];
-		}
-	}
-}
-
